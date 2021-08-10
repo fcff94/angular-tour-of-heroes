a792e288648b16011b34251072596f20ff3b7a50
fcff94 <fati_carol@hotmail.com>
2021-08-10 13:33:25 +0100

    finished data from server;

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,13 +10,15 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  
   heroes: Hero[] = [];
-  
+
   // selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
-  
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
   ngOnInit(): void {
     this.getHeroes();
   }
@@ -27,8 +29,21 @@ export class HeroesComponent implements OnInit {
   // }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
